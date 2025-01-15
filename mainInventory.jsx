9c023382bfd623f3f5793d6f229d379f7d65803e
EleanorHayes <49250551+EleanorHayes@users.noreply.github.com>
2025-01-15 14:06:13 -0800

    Updates

--- mainInventory.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import Inventory from './groupHomeDisplays/inventory.jsx'
-
-const inventoryDomNode = document.getElementById('inventory');
-const inventoryRoot = createRoot(inventoryDomNode); 
-inventoryRoot.render(<Inventory />);
-
--- mainSpellBook.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import SpellBook from './groupHomeDisplays/spellbook.jsx'
-
-const spellDomNode = document.getElementById('spellbook');
-const spellRoot = createRoot(spellDomNode); 
-spellRoot.render(<SpellBook />);
